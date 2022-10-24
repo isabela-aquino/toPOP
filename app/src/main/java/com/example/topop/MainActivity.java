1efd5d2a324177538ba86610898b6ec47c633f57
arquejadalucy <arquejadalucy@gmail.com>
2022-10-24 11:48:20 -0300

    testes para aprender a usar a API

--- app/src/main/java/com/example/topop/MainActivity.java
+++ app/src/main/java/com/example/topop/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.topop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    public ImageView imgBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgBook = (ImageView) findViewById(R.id.imgBook);
+
+        
     }
 }
