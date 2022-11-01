f90649c78e0f241e5db794202d9a279e14cee80a
Isabela Aquino <isabelaaquinom@outlook.com>
2022-10-31 23:38:06 -0300

    Add tela Login e Registrar

--- app/src/main/java/com/example/topop/MainActivity.java
+++ app/src/main/java/com/example/topop/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.topop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button entrar;
+    public  Button registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        entrar = (Button) findViewById(R.id.btnEntrar);
+        registrar = (Button) findViewById(R.id.btnRegistrar);
+
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirTelaEntrar();
+            }
+        });
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirTelaRegistrar();
+            }
+        });
+    }
+
+    public void abrirTelaEntrar(){
+        Intent intent = new Intent(this, activity_login.class);
+        startActivity(intent);
+    }
+
+    public void abrirTelaRegistrar(){
+        Intent intent = new Intent(this, activity_registrar.class);
+        startActivity(intent);
     }
 }
