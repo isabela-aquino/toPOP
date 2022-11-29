b10bb4d2b52068d323efca52e5fdb1007b0e49e6
Isabela Aquino <isabelaaquinom@outlook.com>
2022-11-29 08:21:36 -0300

    Buttons Pagina Login

--- app/src/main/java/com/example/topop/activity/activity_login.java
+++ app/src/main/java/com/example/topop/activity/activity_login.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,6 +18,8 @@ public class activity_login extends AppCompatActivity {
     public Button btnTelaEntrar;
     public TextView textViewRegistrar;
     public ImageView imageViewVoltarLogin;
+    private EditText textEmail;
+    private  EditText textPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,10 @@ public class activity_login extends AppCompatActivity {
         textViewRegistrar = (TextView) findViewById(R.id.TvRegistrese);
 
         imageViewVoltarLogin = (ImageView) findViewById(R.id.voltarLogin);
+
+        textEmail = (EditText) findViewById(R.id.txtEmail);
+
+        textPassword = (EditText) findViewById(R.id.txtPassword);
 
         btnTelaEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +59,8 @@ public class activity_login extends AppCompatActivity {
                startActivity(myIntent);
            }
        });
+
+
     }
 
 }
