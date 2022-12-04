f233d55ba16866e71d473177a748b1f4dec19dc2
jessicasilva-comp <jessicacarolinasilva01@gmail.com>
2022-12-04 11:37:43 -0300

    banco1

--- app/src/main/java/com/example/topop/activity/activity_login.java
+++ app/src/main/java/com/example/topop/activity/activity_login.java
@@ -63,4 +63,5 @@ public class activity_login extends AppCompatActivity {
 
     }
 
+
 }
--- app/src/main/java/com/example/topop/activity/activity_registrar.java
+++ app/src/main/java/com/example/topop/activity/activity_registrar.java
@@ -3,17 +3,24 @@ package com.example.topop.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.topop.R;
+import com.google.android.material.snackbar.Snackbar;
 
 public class activity_registrar extends AppCompatActivity {
 
     public TextView textViewEntrarAgora;
     public ImageView imageViewVoltarRegistrar;
+    private EditText edit_nome, edit_email, edit_senha, edit_confirmasenha;
+    private Button btn_TelaRegistrar;
+    String[] mensagens = {"Preencha todos os campos", "login ok", "Senhas incompatíveis"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,7 @@ public class activity_registrar extends AppCompatActivity {
 
         textViewEntrarAgora = (TextView) findViewById(R.id.TvEntrarAgora);
         imageViewVoltarRegistrar = (ImageView) findViewById(R.id.voltarRegistrar) ;
+        IniciarComponentes();
 
         textViewEntrarAgora.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +46,43 @@ public class activity_registrar extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+        btn_TelaRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = edit_nome.getText().toString();
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+                String ConfirmaSenha = edit_confirmasenha.getText().toString();
+
+                if (nome.isEmpty() || email.isEmpty() || senha.isEmpty() || ConfirmaSenha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.BLACK);
+                    snackbar.setTextColor(Color.WHITE);
+                    snackbar.show();
+
+                } else if (senha!=ConfirmaSenha){
+                    Snackbar snackbar = Snackbar.make(view, mensagens[2], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.BLACK);
+                    snackbar.setTextColor(Color.WHITE);
+                    snackbar.show();
+                } else CadastrarUsuario();
+            }
+        });
     }
+    private void CadastrarUsuario(){
+        String nome = edit_nome.getText().toString();
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+        String ConfirmaSenha = edit_confirmasenha.getText().toString();
+
+    }
+
+    private void IniciarComponentes(){
+        edit_nome = findViewById(R.id.txtUsenamer);
+        edit_email = findViewById(R.id.txtEmail);
+        edit_senha = findViewById(R.id.txtPassword);
+        edit_confirmasenha = findViewById(R.id.txtConfirmPassword);
+        btn_TelaRegistrar = findViewById(R.id.btnTelaRegistrar);
+    }
+
 }
